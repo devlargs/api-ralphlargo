982551d8112282765443b619105a90c891654841
Ralph Largo <devlargs@gmail.com>
2020-08-11 18:39:54 +0800

    added a random generated document

--- routes/quotes.js
+++ routes/quotes.js
@@ -17,4 +17,39 @@ router.get("/", async function (req, res) {
   }
 });
 
+router.get("/random", function (req, res) {
+  try {
+    Quotes.count().exec((err, count) => {
+      const random = Math.floor(Math.random() * count);
+      if (err) {
+        res.send({
+          status: 400,
+          err,
+        });
+      } else {
+        Quotes.findOne()
+          .skip(random)
+          .exec((findErr, data) => {
+            if (findErr) {
+              res.send({
+                err: findErr,
+                status: 400,
+              });
+            } else {
+              res.send({
+                status: 200,
+                data,
+              });
+            }
+          });
+      }
+    });
+  } catch (err) {
+    res.send({
+      status: 400,
+      err,
+    });
+  }
+});
+
 module.exports = router;
