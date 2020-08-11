a09842a8c9f6914bca09555719d150b043c50e29
Ralph Largo <devlargs@gmail.com>
2020-08-11 17:29:01 +0800

    added cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
   useNewUrlParser: true,
@@ -16,7 +17,7 @@ const usersRouter = require("./routes/users");
 const quotesRouter = require("./routes/quotes");
 const app = express();
 
-// view engine setup
+app.use(cors());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
@@ -30,18 +31,13 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/quotes", quotesRouter);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
